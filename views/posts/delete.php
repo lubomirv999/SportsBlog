eff185dac26793733e568c8583ee1c245e53dcf7
ivo_bonev93 <ivo_bonev93@abv.bg>
2016-08-15 16:31:31 +0300

    Delete post functionality added

--- views/posts/delete.php
+++ views/posts/delete.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 10.8.2016 г.
- * Time: 23:14 ч.
- */
+<?php $this->title = 'Delete Post'; ?>
+<h1>Are you sure you want to delete this post?</h1>
+<form method="post">
+    <div>Title:</div>
+    <input type="text" value="<?=htmlspecialchars($this->post['title'])?>" disabled/>
+    <div>Content:</div>
+    <textarea rows="10" disabled><?=htmlspecialchars($this->post['content'])?></textarea>
+    <div>Date:</div>
+    <input type="text" value="<?=htmlspecialchars($this->post['date'])?>" disabled/>
+    <div><input type="submit" value="Delete"/><a href="<?=APP_ROOT?>/posts">[Cancel]</a> </div>
+    
+</form>
