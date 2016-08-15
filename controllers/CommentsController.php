396f5502fde7d8d206697140eba46832d1dd0045
Kamen <kamenminkov.com@abv.bg>
2016-08-15 16:58:35 +0300

    new controllers

---
+++ controllers/CommentsController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kamen
+ * Date: 15.8.2016 г.
+ * Time: 15:20
+ */
+class CommentsController extends BaseController
+{
+    function onInit()
+    {
+        $this->authorize();
+    }
+
+    public function create_comment(int $id)
+    {
+
+    }
+
+    public function edit_comment(int $id)
+    {
+
+    }
+
+    public function delete_comment(int $id)
+    {
+
+    }
+}
---
+++ models/CommentsModel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kamen
+ * Date: 15.8.2016 г.
+ * Time: 15:22
+ */
+class CommentsModel extends BaseModel
+{
+    public function create_comment(string $content, int $user_id, int $post_id)
+    {
+
+    }
+
+    public function delete_comment(int $id)
+    {
+
+    }
+}
