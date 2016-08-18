56124f7593d6141a701f59a48f6a2d806025d2bb
Kamen <kamenminkov.com@abv.bg>
2016-08-18 18:41:58 +0300

    Reapairing broken code

--- models/PostsModel.php
+++ models/PostsModel.php
@@ -10,18 +10,18 @@ class PostsModel extends BaseModel
 {
     public function getAll() : array
     {
-     $statement = self::$db->query("SELECT posts.Id, title, content, FullName, date " .
-         "FROM posts LEFT JOIN users ON posts.user_id = users.ID " . "ORDER BY date DESC ");
+        $statement = self::$db->query("SELECT posts.Id, title, content, FullName, date " .
+            "FROM posts LEFT JOIN users ON posts.user_id = users.ID " . "ORDER BY date DESC ");
         return $statement->fetch_all(MYSQLI_ASSOC);
 
     }
 
     public function getById(int $id)
     {
-        $statement = self::$db->prepare("SELECT users.UserName, posts.user_id, posts.date, posts.title, posts.content, posts.Id ".
-            "FROM posts LEFT JOIN users ON posts.user_id = users.ID ".
+        $statement = self::$db->prepare("SELECT users.UserName, posts.user_id, posts.date, posts.title, posts.content, posts.Id " .
+            "FROM posts LEFT JOIN users ON posts.user_id = users.ID " .
             "WHERE posts.Id = ? ");
-        $statement->bind_param("i",$id);
+        $statement->bind_param("i", $id);
         $statement->execute();
         $result = $statement->get_result()->fetch_assoc();
         return $result;
@@ -30,16 +30,15 @@ class PostsModel extends BaseModel
     public function create(string $title, string $content, int $user_id) : bool
     {
         $statement = self::$db->prepare("INSERT INTO posts (title,content,user_id) VALUES (?,?,?) ");
-        $statement->bind_param("ssi",$title,$content,$user_id);
+        $statement->bind_param("ssi", $title, $content, $user_id);
         $statement->execute();
-        if ($statement->affected_rows==0)
-        {
+        if ($statement->affected_rows == 0) {
             return false;
         }
         return self::$db->insert_id;
     }
 
-    public function edit(string $id,string $title, string $content, string $date, int $user_id) : bool
+    public function edit(string $id, string $title, string $content, string $date, int $user_id) : bool
     {
         $statement = self::$db->prepare("UPDATE posts SET title = ?, content = ?, date = ?, user_id = ? WHERE id = ?");
         $statement->bind_param("sssii", $title, $content, $date, $user_id, $id);
@@ -51,37 +50,39 @@ class PostsModel extends BaseModel
     public function delete(int $id) : bool
     {
         $statement = self::$db->prepare("DELETE FROM posts WHERE id = ?");
-        $statement->bind_param("i",$id);
+        $statement->bind_param("i", $id);
         $statement->execute();
         return $statement->affected_rows == 1;
     }
 
-    public function create_comment (string $content, int $user_id, int $post_id)
+    public function create_comment(string $content, int $user_id, int $post_id)
     {
         $statement = self::$db->prepare("INSERT INTO comments (content, post_id,user_id) VALUES (?,?,?) ");
-        $statement -> bind_param("sii", $content,$post_id,$user_id);
-        $statement -> execute();
-        if ($statement->affected_rows==0)
-        {
+        $statement->bind_param("sii", $content, $post_id, $user_id);
+        $statement->execute();
+        if ($statement->affected_rows == 0) {
             return false;
         }
         return self::$db->insert_id;
     }
 
-    public function listComments ($id)
+    public function listComments($id)
     {
         $statement = self::$db->prepare(
-            "SELECT post_id, comments.content, comments.date, users.UserName, comments.ID ".
-            "FROM comments LEFT JOIN posts ON comments.post_id = posts.Id LEFT JOIN users ON comments.user_id = users.ID ".
-            "WHERE posts.Id = ? ".
+            "SELECT post_id, comments.content, comments.date, users.UserName, comments.ID " .
+            "FROM comments LEFT JOIN posts ON comments.post_id = posts.Id LEFT JOIN users ON comments.user_id = users.ID " .
+            "WHERE posts.Id = ? " .
             "ORDER BY date DESC ");
-        $statement-> bind_param("i", $id);
+        $statement->bind_param("i", $id);
         $statement->execute();
         return $statement->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function delete_comment(int $id)
+    public function deleteComment(int $id)
     {
-
+        $statement = self::$db->prepare("DELETE FROM comments WHERE comments.ID=? ");
+        $statement->bind_param("i", $id);
+        $statement->execute();
+        return $statement->affected_rows == 1;
     }
 }
