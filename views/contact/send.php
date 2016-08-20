d7faf4d163e3444b7f92190622900856d8ceaca8
lubomirv999 <lubomir999@abv.bg>
2016-08-20 15:48:32 +0300

    Contact page layout adjust and working!

--- views/contact/send.php
+++ views/contact/send.php
@@ -1,16 +1,14 @@
 <?php $this->title = 'Contact us'; ?>
 <form method="post" action="<?=APP_ROOT?>/contact/send">
-    <div>Want to get in touch with us? Leave us a message below to contact the team and we will get back to you within 24 hours!</div>
-    <br>
+    <div id="contact">Want to get in touch with us? Leave us a message below to contact the team and we will get back to you within 24 hours!</div>
     <div>
-        <label for="content">Message</label>
         <div>
             <textarea rows="10" cols="70" name="content" id="contentBox" placeholder="Your text here..."></textarea>
         </div>
     </div>
     <div>
         <div>
-            <input type="submit" value="Send message" />
+            <input type="submit" value="Send message"/>
         </div>
     </div>
 </form>
