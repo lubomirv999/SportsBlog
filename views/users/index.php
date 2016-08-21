f0edbbc46d95ae06c572340b06e2f0464667ebbc
NaskoTrenchev <naskotren4ev@abv.bg>
2016-08-21 23:09:28 +0300

    Users Styles

--- views/users/index.php
+++ views/users/index.php
@@ -1,16 +1,14 @@
 <?php $this->title = 'Users'; ?>
 <form action="">
 <?php foreach ($this->users as $user) : ?>
-
-<table>
- <tr>
-  <th>Username: </th>
-  <th>FullName: </th>
- </tr>
- <tr>
-  <td><a href="<?=APP_ROOT?>/posts"><?=$user['UserName']?></a></td><br>
-  <td><?=$user['FullName']?></td>
- </tr>
-</table>
-<?php endforeach;?>
+    <div class="users">
+  <div class="right"> Username:
+  <a href =" <?=APP_ROOT?>/posts"><?=$user['UserName']?></a><br>
+  </div>
+  <div class="left">FullName:
+   <?=$user['FullName']?>
+  </div>
+     </div>
+ <?php endforeach;?>
 </form>
+
