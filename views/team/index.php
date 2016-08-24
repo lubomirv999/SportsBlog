45db37a92c8ce7643d6768723b8faaa8d5ecb8bf
lubomirv999 <lubomir999@abv.bg>
2016-08-24 13:49:07 +0300

    About us page done! Add some info about you!

--- views/team/index.php
+++ views/team/index.php
@@ -1,1 +1,79 @@
-<h2 align="center">It's empty here.</h2>
+<div class="wrapper row2">
+    <div id="container" class="clear">
+        <!-- ####################################################################################################### -->
+        <!-- ####################################################################################################### -->
+        <!-- ####################################################################################################### -->
+        <!-- ####################################################################################################### -->
+        <div id="about-us" class="clear">
+            <!-- ####################################################################################################### -->
+            <section id="about-intro" class="clear">
+                <p>Write some info about all of us!</p>
+                <p>Write some info about all of us - second paragraph</p>
+            </section>
+            <!-- ####################################################################################################### -->
+            <section id="team">
+                <ul class="clear">
+                    <li class="one_quarter">
+                        <figure><img src="<?=APP_ROOT?>/content/images/ProfilePic_Kamen.jpg" alt="">
+                            <figcaption>
+                                <p class="team_name">Камен Минков</p>
+                                <p class="team_title">Back End</p>
+                                <p class="team_description">Write Some info about you!</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="one_quarter">
+                        <figure><img src="<?=APP_ROOT?>/content/images/ProfilePic_Lubo.jpg" alt="">
+                            <figcaption>
+                                <p class="team_name">Любомир Вълев</p>
+                                <p class="team_title">Front End</p>
+                                <p class="team_description">Write Some info about you!</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="one_quarter">
+                        <figure><img src="<?=APP_ROOT?>/content/images/ProfilePic_IvoBarakov.jpg" alt="">
+                            <figcaption>
+                                <p class="team_name">Ивайло Бараков</p>
+                                <p class="team_title">Back End</p>
+                                <p class="team_description">Write Some info about you!</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="one_quarter">
+                        <figure><img src="<?=APP_ROOT?>/content/images/ProfilePic_Nasko.jpg" alt="">
+                            <figcaption>
+                                <p class="team_name">Наско Тренчев</p>
+                                <p class="team_title">Front End</p>
+                                <p class="team_description">Write Some info about you!</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="one_quarter">
+                        <figure><img src="<?=APP_ROOT?>/content/images/ProfilePic_IvoTraykov.jpg" alt="">
+                            <figcaption>
+                                <p class="team_name">Владимир Трайков</p>
+                                <p class="team_title">Front End</p>
+                                <p class="team_description">Write Some info about you!</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="one_quarter first">
+                        <figure><img src="<?=APP_ROOT?>/content/images/ProfilePic_IvoBonev.jpg" alt="">
+                            <figcaption>
+                                <p class="team_name">Ивайло Бонев</p>
+                                <p class="team_title">Back End</p>
+                                <p class="team_description">Write Some info about you!</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                </ul>
+            </section>
+            <!-- ####################################################################################################### -->
+        </div>
+        <!-- ####################################################################################################### -->
+        <!-- ####################################################################################################### -->
+        <!-- ####################################################################################################### -->
+        <!-- ####################################################################################################### -->
+    </div>
+</div>
