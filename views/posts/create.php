e47df09f6cd15bd3fd347edbc1d2b2db3859dff8
ivo_bonev93 <ivo_bonev93@abv.bg>
2016-08-28 22:58:32 +0300

    Upload images functionality created

--- views/posts/create.php
+++ views/posts/create.php
@@ -2,6 +2,14 @@
 <script type="text/javascript" src="<?=APP_ROOT?>/ckeditor/ckeditor.js"></script>
 
 <form name="createPost" method="post" action="<?=APP_ROOT?>/posts/create" enctype="multipart/form-data">
+    <select name="category" id="category">
+        <option value="0" selected="selected">Select a category</option>
+        <option value="Football">Football</option>
+        <option value="Athletics">Athletics</option>
+        <option value="Swimming">Swimming</option>
+        <option value="Wrestling">Wrestling</option>
+        <option value="Others">Others</option>
+    </select>
     <div>
         <input id="title" type="text" placeholder="Title" name="title" />
     </div>
